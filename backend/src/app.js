cdc80e78f40bfd6f22234c82d6de56e57fc3b242
riyo3350g <yechouben@gmail.com>
2026-02-10 14:42:59 +0100

    Day 2: express server setup

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "Job Tracker API is running!" });
+});
+
+module.exports = app;
