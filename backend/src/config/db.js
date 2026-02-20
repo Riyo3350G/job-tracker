5ef5b7a54b55a01e9e09b2658793cde828550f98
riyo3350g <yechouben@gmail.com>
2026-02-20 15:18:14 +0000

    Fix pool variable is missing

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,24 +1,20 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-if (process.env.DATABASE_URL) {
-  // For production render
-  let pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-} else {
-  // For local development
-  let pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-  });
-}
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT || 5432,
+      }
+);
 
 pool.on("connect", () => {
   console.log("Connected to the database");
