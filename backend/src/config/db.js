1990e55a312abf84ccf64b97480c8770ee640ed9
riyo3350g <yechouben@gmail.com>
2026-02-20 11:27:01 +0000

    Config db for production

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,24 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
+if (process.env.DATABASE_URL) {
+  // For production render
+  let pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  // For local development
+  let pool = new Pool({
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
     database: process.env.DB_NAME,
-});
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 
 pool.on("connect", () => {
   console.log("Connected to the database");
